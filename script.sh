3959b0084b133e1dc5324958eb8351aa3b34bddf
Yuriy Vysotskyi <165578864+geeksblack@users.noreply.github.com>
2025-01-11 00:29:39 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,7 @@
-#! /bin/bash
-
-echo "enter the name"
-read num
-
-if[$(($num%2))-eq 0] then
-echo "The num is even"
-else
-echo "THe num is odd"
-fi
-
-echo "the file is in directory"
-
-mkdir -p /backup
-for file in *.txt 
-cp file backup/
-echo "the $file coppied in backup/"
-done
-
+#!/bin/bash
+SRC=~/project/src
+BACKUP=~/project/backup
+mkdir -p $BACKUP
+cp -r $SRC/* $BACKUP/
+echo "The reservation of file finished on the $(date)" >> ~/backup.log
 
